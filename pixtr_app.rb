ebe68fa1a09f6d0ba6e46a0bd5cf74b1cf1acbe0
Mike Murphy <mikeymurph77@gmail.com>
2014-06-11 16:41:51 -0400

    Render index to show links

--- pixtr_app.rb
+++ pixtr_app.rb
@@ -3,28 +3,25 @@ require "active_record"
 require "pg"
 
 ActiveRecord::Base.establish_connection(
-	adapter: "postgresql",
-	database: "photo_gallery"
-	)
+  adapter: "postgresql",
+  database: "photo_gallery"
+  )
 
 class Gallery < ActiveRecord::Base
 end
 
+class Image < ActiveRecord::Base
+end
+
 get "/" do
-	@galleries = Gallery.all
-	erb :index
+  @galleries = Gallery.all
+  erb :index
 end
 
 
-get "/:gallery_name" do
-  cat_images_in_app_rb = ["colonel_meow.jpg", "grumpy_cat.png"]
-  dog_images_in_app_rb = ["shibe.png"]
+get "/galleries/:id" do
+  @gallery = Gallery.find(params[:id])
+  @images = Image.where(gallery_id: @gallery.id)
 
-  gallery_name = params[:gallery_name]
-  erb(gallery_name.to_sym, {
-    locals: {
-      cat_images: cat_images_in_app_rb,
-      dog_images: dog_images_in_app_rb
-    }
-  })
+  erb :show
 end
